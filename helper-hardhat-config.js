42cae486c706a3f3e49703cfc2ee5aaf7778c07b
oktaykurt <oktaykurt2000@gmail.com>
2023-02-23 14:27:37 +0300

    Fixes hardhat deploy issue

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,6 +1,20 @@
 const { ethers } = require("hardhat");
 
 const networkConfig = {
+  default: {
+    name: "hardhat",
+    keepersUpdateInterval: "30",
+  },
+
+  31337: {
+    name: "localhost",
+    subscriptionId: "588",
+    gasLane:
+      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
+  },
 
   5: {
     name: "goerli",
@@ -14,6 +28,7 @@ const networkConfig = {
   },
 };
 
+const developmentChains = ["hardhat", "localhost"];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 const frontEndContractsFile =
   "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json";
